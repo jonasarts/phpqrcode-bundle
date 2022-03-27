a4c34e5e40f8cbc5bee48cb41ce3ad56f7c1be48
Jonas Hauser <hauser@hasch.io>
2022-03-27 16:05:12 +0200

    Added type safety to bundle

--- src/PHPQRCodeBundle.php
+++ src/PHPQRCodeBundle.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace jonasarts\Bundle\PHPQRCodeBundle;
 
 use jonasarts\Bundle\PHPQRCodeBundle\DependencyInjection\PHPQRCodeExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PHPQRCodeBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new PHPQRCodeExtension();
     }
